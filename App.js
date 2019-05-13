594f0198a2c1b56ce04c7824002a36377246d0e0
Jazz Jiang <526446847@qq.com>
2019-05-13 17:41:39 +0800

    redux

--- App.js
+++ App.js
@@ -1,47 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import friendReducer from './client/Reducers/FriendReducer';
 import AppNavigator from './client/AppNavigator';
 
+const store = createStore(friendReducer);
+
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      possibleFriends: [
-        'Allie',
-        'Gator',
-        'Lizzie',
-      ],
-      currentFriends: [],
-    }
-  }
-
-  addFriend = (index) => {
-    const {
-      currentFriends,
-      possibleFriends,
-    } = this.state
-
-    // Pull friend out of possibleFriends
-    const addedFriend = possibleFriends.splice(index, 1)
-
-    // And put friend in currentFriends
-    currentFriends.push(addedFriend)
-
-    // Finally, update our app state
-    this.setState({
-      currentFriends,
-      possibleFriends,
-    })
-  }
-
   render() {
     return (
-      <AppNavigator
-        screenProps={{
-          currentFriends: this.state.currentFriends,
-          possibleFriends: this.state.possibleFriends,
-          addFriend: this.addFriend,
-        }}
-      />
+      <Provider store={ store } >
+        <AppNavigator />
+      </Provider>
     );
   }
 }
